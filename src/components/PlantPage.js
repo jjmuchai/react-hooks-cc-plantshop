9773af86b5f9eeb59f155aaca199ef2485f156ea
jjmuchai <jjmuchai151@gmail.com>
2025-04-28 12:50:29 +0300

    My updates to the project

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data))
+      .catch((error) => console.error("Error fetching plants:", error));
+  }, []);
+
+  const handleAddPlant = (newPlant) => {
+    setPlants((plants) => [...plants, newPlant]);
+  };
+
+  const handleSearchChange = (searchTerm) => {
+    setSearchQuery(searchTerm);
+  };
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search onSearchChange={handleSearchChange} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
